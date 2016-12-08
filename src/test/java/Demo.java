efc05b5cb0f5d7d908d4587277d945fc35cad825
pingwin <jaloszewski.adrian@gmail.com>
2016-12-08 23:31:27 +0100

    removed binary files

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -8,23 +8,23 @@ public class Demo {
     // goal string
     private static final String optimal = "abcdefghijklmnopqrstuvwxyz";
 
-    private static int hammingDistance(String first, String second){
+    private static int hammingDistance(String first, String second) {
         if (first.length() != second.length())
             return -1;
 
         int res = 0;
-        for(int i = 0; i < first.length(); i++)
+        for (int i = 0; i < first.length(); i++)
             res += (first.charAt(i) == second.charAt(i)) ? 0 : 1;
         return res;
     }
 
     // change one random character to another randomly generated
-    private static String mutate(String str){
+    private static String mutate(String str) {
         Random r1 = new Random();
         int position = r1.nextInt(str.length());
 
         Random r2 = new Random();
-        char ch = (char)(r2.nextInt(26) + 'a');
+        char ch = (char) (r2.nextInt(26) + 'a');
 
         StringBuilder newStr = new StringBuilder(str);
         newStr.setCharAt(position, ch);
@@ -34,7 +34,7 @@ public class Demo {
 
     public static void main(String[] args) {
         // initialize the problem
-        SimulatedAnnealingSolver<String> solver = new SimulatedAnnealingSolver<String>(
+        SimulatedAnnealingSolver<String> solver = new SimulatedAnnealingSolver<>(
                 "kmgniwsodfhydmsferinoqtngh",
                 Demo::mutate,
                 (String str) -> (double) hammingDistance(str, optimal),
