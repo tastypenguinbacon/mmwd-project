d72c86c77c735949467caf03362b85b4ceb47a30
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2017-01-15 18:20:27 +0100

    Minor fixes.

--- src/test/java/pl/edu/agh/student/simulatedannealing/model/PizzaDelivererTest.java
+++ src/test/java/pl/edu/agh/student/simulatedannealing/model/PizzaDelivererTest.java
@@ -1,8 +1,16 @@
 package pl.edu.agh.student.simulatedannealing.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,6 +21,30 @@ import static org.junit.Assert.assertTrue;
  * Created by Ja on 2017-01-04.
  */
 public class PizzaDelivererTest {
+    @Before
+    public void setUp() throws IOException {
+        Point delivererPosition = new Point(-1, 0);
+        PizzaDeliverer deliverer = new PizzaDeliverer(delivererPosition);
+        List<Pizza> pizzasWeAreObligatedToDeliver = new LinkedList<>();
+        //List<Pizza> pizzasWeCouldDeliver = new LinkedList<>();
+        List<Point> route = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            Pizza newPizza = new Pizza(new Point(i, i), new Point(i, i));
+            Pizza couldBeDelivered = new Pizza(new Point(i, i), new Point(i+1, i+1));
+            pizzasWeAreObligatedToDeliver.add(newPizza);
+            //pizzasWeCouldDeliver.add(couldBeDelivered);
+            route.add(new Point(i,i));
+
+            deliverer.addPizza(couldBeDelivered);
+        }
+        route.add(new Point(3,3));
+
+        deliverer.setRoute(route);
+        deliverer.setPizzasWeAreObligatedToDeliver(pizzasWeAreObligatedToDeliver);
+
+        this.deliverer = deliverer;
+    }
+
     @Test
     public void checkThatInsertionIsNotPossibleTest() {
         //todo test for insertion
@@ -66,30 +98,6 @@ public class PizzaDelivererTest {
     }
 
     private PizzaDeliverer deliverer;
-
-    @Before
-    public void setUp() {
-        Point delivererPosition = new Point(-1, 0);
-        PizzaDeliverer deliverer = new PizzaDeliverer(delivererPosition);
-        List<Pizza> pizzasWeAreObligatedToDeliver = new LinkedList<>();
-        //List<Pizza> pizzasWeCouldDeliver = new LinkedList<>();
-        List<Point> route = new LinkedList<>();
-        for (int i = 0; i < 3; i++) {
-            Pizza newPizza = new Pizza(new Point(i, i), new Point(i, i));
-            Pizza couldBeDelivered = new Pizza(new Point(i, i), new Point(i+1, i+1));
-            pizzasWeAreObligatedToDeliver.add(newPizza);
-            //pizzasWeCouldDeliver.add(couldBeDelivered);
-            route.add(new Point(i,i));
-
-            deliverer.addPizza(couldBeDelivered);
-        }
-        route.add(new Point(3,3));
-
-        deliverer.setRoute(route);
-        deliverer.setPizzasWeAreObligatedToDeliver(pizzasWeAreObligatedToDeliver);
-
-        this.deliverer = deliverer;
-    }
 
     @Test
     public void isAbleToCollectThePizzasTest() {
