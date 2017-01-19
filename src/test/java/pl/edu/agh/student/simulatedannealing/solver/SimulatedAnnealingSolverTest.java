f9407b0827d3710bddb88f231d4de43f42f18b2c
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2017-01-19 23:32:05 +0100

    Fixed test cases.
    Changed test cases to reflect changes in program logic. All tests should pass now.

--- src/test/java/pl/edu/agh/student/simulatedannealing/solver/SimulatedAnnealingSolverTest.java
+++ src/test/java/pl/edu/agh/student/simulatedannealing/solver/SimulatedAnnealingSolverTest.java
@@ -18,7 +18,7 @@ public class SimulatedAnnealingSolverTest {
     public void givenQuadraticEquationFindMinimum() {
         Mutator<Double> mutator = current -> current + new Random().nextGaussian() / 10;
         Temperature temperature = new ExponentiallyDecreasingTemperature(100, 0.997);
-        Function<Double, Double> objectiveFunction = x -> -Math.pow(x - 3, 2) + 1;
+        Function<Double, Double> objectiveFunction = x -> -Math.pow(x - 3, 2) + 200;
         SimulatedAnnealingSolver<Double> solver = new SimulatedAnnealingSolver<>(mutator, temperature, objectiveFunction);
         double solution = solver.solve(-10.0, 3000);
         System.out.println(solution);
